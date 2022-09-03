fa657b1c459623f417f881bc007bc529c1d27243
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2022-09-03 01:46:31 -0500

    Storage Services completed

--- src/app/core/services/storage.service.ts
+++ src/app/core/services/storage.service.ts
@@ -8,16 +8,42 @@ export class StorageService {
   geographicalData: Observable<any>;
   private geographicalData$: BehaviorSubject<any>;
 
+  insulatorData: Observable<any>;
+  private insulatorData$: BehaviorSubject<any>;
+
+  mechanicalCalculation: Observable<any>;
+  private mechanicalCalculation$: BehaviorSubject<any>;
+
+  lineTransmissionData: Observable<any>;
+  private lineTransmissionData$: BehaviorSubject<any>;
+
   constructor() {
     this.geographicalData$ = new BehaviorSubject(null);
     this.geographicalData = this.geographicalData$.asObservable();
-  }
 
-  getGeographicalData() {
-    return this.geographicalData$.asObservable();
+    this.insulatorData$ = new BehaviorSubject(null);
+    this.insulatorData = this.insulatorData$.asObservable();
+
+    this.lineTransmissionData$ = new BehaviorSubject(null);
+    this.lineTransmissionData = this.lineTransmissionData$.asObservable();
+
+    this.mechanicalCalculation$ = new BehaviorSubject(null);
+    this.mechanicalCalculation = this.mechanicalCalculation$.asObservable();
   }
 
   setGeographicalData(data: any) {
     this.geographicalData$.next(data);
+  }
+
+  setInsulatorData(data: any) {
+    this.insulatorData$.next(data);
+  }
+
+  setMechanicalCalculationData(data: any) {
+    this.mechanicalCalculation$.next(data);
+  }
+
+  setLineTransmissionData(data: any) {
+    this.lineTransmissionData$.next(data);
   }
 }
