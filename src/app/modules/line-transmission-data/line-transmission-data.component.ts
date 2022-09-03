afd10af4ac1ae96e4561c9d4d0f82515d9f3ecd3
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2022-09-03 15:20:31 -0500

    Separation Base Database into separated const files

--- src/app/modules/line-transmission-data/line-transmission-data.component.ts
+++ src/app/modules/line-transmission-data/line-transmission-data.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { StorageService } from 'src/app/core/services/storage.service';
+import { LINE_TRANSMISION_DATA_BASE } from 'src/app/shared/lineTransmissionData.cst';
 
 @Component({
   selector: 'app-line-transmission-data',
@@ -6,9 +11,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-transmission-data.component.scss'],
 })
 export class LineTransmissionDataComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  maximumTensionList = LINE_TRANSMISION_DATA_BASE;
+  conmutationBasicLevelList = [];
+  isolationBasicLevelList = [];
+  contaminationLevelList = [];
+  precipitationIntensityList = [];
+
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly storageService: StorageService,
+    private readonly router: Router
+  ) {
+    this.form = this.fb.group({
+      maximumTensionId: [null, [Validators.required]],
+      conmutationBasicLevelId: [null, [Validators.required]],
+      isolationBasicLevelId: [null, [Validators.required]],
+      contaminationLevelId: [null, [Validators.required]],
+      precipitationIntensityId: [null, [Validators.required]],
+    });
+
+    /*
+     * Si llenan la TENSION MAXIMA
+     */
+    this.form
+      .get('maximumTensionId')
+      .valueChanges.pipe(filter((value) => value))
+      .subscribe((value) => {
+        this.form.patchValue({
+          conmutationBasicLevelId: null,
+          isolationBasicLevelId: null,
+        });
+
+        this.conmutationBasicLevelList = this.maximumTensionList.find(
+          (t) => t.id == value
+        ).conmutationBasicLevelList;
+
+        console.log(this.maximumTensionList);
+
+        console.log(this.conmutationBasicLevelList);
+
+        if (this.conmutationBasicLevelList.length == 1) {
+          this.form.patchValue({
+            conmutationBasicLevelId: this.conmutationBasicLevelList[0].id,
+          });
+        }
+      });
+
+    /*
+     * Si llenan el NIVEL BASICO DE CONMUTACION
+     */
+    this.form
+      .get('conmutationBasicLevelId')
+      .valueChanges.pipe(filter((value) => value))
+      .subscribe((valueCon) => {
+        this.form.patchValue({
+          isolationBasicLevelId: null,
+        });
+
+        console.log({ valueCon });
+        console.log(
+          this.conmutationBasicLevelList.find((t) => t.id == valueCon)
+        );
+
+        this.isolationBasicLevelList = this.conmutationBasicLevelList.find(
+          (t) => t.id == valueCon
+        ).isolationBasicLevelsList;
+
+        if (this.isolationBasicLevelList.length == 1) {
+          this.form.patchValue({
+            isolationBasicLevelId: this.isolationBasicLevelList[0].id,
+          });
+        }
+      });
+
+    this.storageService.lineTransmissionData.subscribe((data) => {
+      if (data) {
+        this.form.patchValue(data);
+      }
+    });
+  }
 
   ngOnInit() {}
 
+  saveForm() {
+    this.storageService.setLineTransmissionData(this.form.getRawValue());
+    this.storageService.setLineTransmissionDataIsComplete(true);
+
+    this.router.navigate(['/']);
+  }
 }
---
+++ src/app/shared/lineTransmissionData.cst.ts
@@ -0,0 +1,634 @@
+export const LINE_TRANSMISION_DATA_BASE = [
+  {
+    id: 1,
+    name: '3.6',
+    value: 3.6,
+    conmutationBasicLevelList: [
+      {
+        name: '10',
+        id: 1,
+        value: 10,
+        isolationBasicLevelsList: [
+          {
+            name: '20',
+            id: 1,
+            value: 20,
+          },
+          {
+            name: '40',
+            id: 2,
+            value: 40,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: '7.2',
+    value: 7.2,
+    conmutationBasicLevelList: [
+      {
+        id: 2,
+        name: '20',
+        value: 20,
+        isolationBasicLevelsList: [
+          {
+            id: 3,
+            name: '40',
+            value: 40,
+          },
+          {
+            id: 4,
+            name: '60',
+            value: 60,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: '12',
+    value: 12,
+    conmutationBasicLevelList: [
+      {
+        id: 3,
+        name: '28',
+        value: 28,
+        isolationBasicLevelsList: [
+          {
+            id: 5,
+            name: '60',
+            value: 60,
+          },
+          {
+            id: 6,
+            name: '75',
+            value: 75,
+          },
+          {
+            id: 7,
+            name: '95',
+            value: 95,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: '17.5',
+    value: 17.5,
+    conmutationBasicLevelList: [
+      {
+        id: 4,
+        name: '38',
+        value: 38,
+        isolationBasicLevelsList: [
+          {
+            id: 8,
+            name: '75',
+            value: 75,
+          },
+          {
+            id: 9,
+            name: '95',
+            value: 95,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: '24',
+    value: 24,
+    conmutationBasicLevelList: [
+      {
+        id: 5,
+        name: '50',
+        value: 50,
+        isolationBasicLevelsList: [
+          {
+            id: 10,
+            name: '95',
+            value: 95,
+          },
+          {
+            id: 11,
+            name: '125',
+            value: 125,
+          },
+          {
+            id: 12,
+            name: '145',
+            value: 145,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: '36',
+    value: 36,
+    conmutationBasicLevelList: [
+      {
+        id: 6,
+        name: '70',
+        value: 70,
+        isolationBasicLevelsList: [
+          {
+            id: 13,
+            name: '145',
+            value: 145,
+          },
+          {
+            id: 14,
+            name: '170',
+            value: 170,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: '52',
+    value: 52,
+    conmutationBasicLevelList: [
+      {
+        id: 7,
+        name: '95',
+        value: 95,
+        isolationBasicLevelsList: [
+          {
+            id: 15,
+            name: '250',
+            value: 250,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: '72.5',
+    value: 72.5,
+    conmutationBasicLevelList: [
+      {
+        id: 8,
+        name: '140',
+        value: 140,
+        isolationBasicLevelsList: [
+          {
+            id: 16,
+            name: '325',
+            value: 325,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: '123',
+    value: 123,
+    conmutationBasicLevelList: [
+      {
+        id: 9,
+        name: '185',
+        value: 185,
+        isolationBasicLevelsList: [
+          {
+            id: 17,
+            name: '450',
+            value: 450,
+          },
+        ],
+      },
+      {
+        id: 10,
+        name: '230',
+        value: 230,
+        isolationBasicLevelsList: [
+          {
+            id: 18,
+            name: '550',
+            value: 550,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: '145',
+    value: 145,
+    conmutationBasicLevelList: [
+      {
+        id: 11,
+        name: '185',
+        value: 185,
+        isolationBasicLevelsList: [
+          {
+            id: 19,
+            name: '450',
+            value: 450,
+          },
+        ],
+      },
+      {
+        id: 12,
+        name: '230',
+        value: 230,
+        isolationBasicLevelsList: [
+          {
+            id: 20,
+            name: '550',
+            value: 550,
+          },
+        ],
+      },
+      {
+        id: 13,
+        name: '275',
+        value: 275,
+        isolationBasicLevelsList: [
+          {
+            id: 21,
+            name: '650',
+            value: 650,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: '170',
+    value: 170,
+    conmutationBasicLevelList: [
+      {
+        id: 14,
+        name: '230',
+        value: 230,
+        isolationBasicLevelsList: [
+          {
+            id: 22,
+            name: '550',
+            value: 550,
+          },
+        ],
+      },
+      {
+        id: 15,
+        name: '275',
+        value: 275,
+        isolationBasicLevelsList: [
+          {
+            id: 23,
+            name: '650',
+            value: 650,
+          },
+        ],
+      },
+      {
+        id: 16,
+        name: '325',
+        value: 325,
+        isolationBasicLevelsList: [
+          {
+            id: 24,
+            name: '750',
+            value: 750,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: '245',
+    value: 245,
+    conmutationBasicLevelList: [
+      {
+        id: 17,
+        name: '275',
+        value: 275,
+        isolationBasicLevelsList: [
+          {
+            id: 25,
+            name: '650',
+            value: 650,
+          },
+        ],
+      },
+      {
+        id: 18,
+        name: '325',
+        value: 325,
+        isolationBasicLevelsList: [
+          {
+            id: 26,
+            name: '750',
+            value: 750,
+          },
+        ],
+      },
+      {
+        id: 19,
+        name: '360',
+        value: 360,
+        isolationBasicLevelsList: [
+          {
+            id: 27,
+            name: '850',
+            value: 850,
+          },
+        ],
+      },
+      {
+        id: 20,
+        name: '395',
+        value: 395,
+        isolationBasicLevelsList: [
+          {
+            id: 28,
+            name: '950',
+            value: 950,
+          },
+        ],
+      },
+      {
+        id: 21,
+        name: '460',
+        value: 460,
+        isolationBasicLevelsList: [
+          {
+            id: 29,
+            name: '1050',
+            value: 1050,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 13,
+    name: '300',
+    value: 300,
+    conmutationBasicLevelList: [
+      {
+        id: 22,
+        name: '750',
+        value: 750,
+        isolationBasicLevelsList: [
+          {
+            id: 30,
+            name: '850',
+            value: 850,
+          },
+          {
+            id: 31,
+            name: '950',
+            value: 950,
+          },
+        ],
+      },
+      {
+        id: 23,
+        name: '850',
+        value: 850,
+        isolationBasicLevelsList: [
+          {
+            id: 32,
+            name: '950',
+            value: 950,
+          },
+          {
+            id: 33,
+            name: '1050',
+            value: 1050,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 14,
+    name: '362',
+    value: 362,
+    conmutationBasicLevelList: [
+      {
+        id: 24,
+        name: '850',
+        value: 850,
+        isolationBasicLevelsList: [
+          {
+            id: 34,
+            name: '950',
+            value: 950,
+          },
+          {
+            id: 35,
+            name: '1050',
+            value: 1050,
+          },
+        ],
+      },
+      {
+        id: 25,
+        name: '950',
+        value: 950,
+        isolationBasicLevelsList: [
+          {
+            id: 36,
+            name: '1050',
+            value: 1050,
+          },
+          {
+            id: 37,
+            name: '1175',
+            value: 1175,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 15,
+    name: '420',
+    value: 420,
+    conmutationBasicLevelList: [
+      {
+        id: 26,
+        name: '850',
+        value: 850,
+        isolationBasicLevelsList: [
+          {
+            id: 38,
+            name: '1050',
+            value: 1050,
+          },
+          {
+            id: 39,
+            name: '1175',
+            value: 1175,
+          },
+        ],
+      },
+      {
+        id: 27,
+        name: '950',
+        value: 950,
+        isolationBasicLevelsList: [
+          {
+            id: 40,
+            name: '1175',
+            value: 1175,
+          },
+          {
+            id: 41,
+            name: '1300',
+            value: 1300,
+          },
+        ],
+      },
+      {
+        id: 28,
+        name: '1050',
+        value: 1050,
+        isolationBasicLevelsList: [
+          {
+            id: 42,
+            name: '1300',
+            value: 1300,
+          },
+          {
+            id: 43,
+            name: '1425',
+            value: 1425,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 16,
+    name: '525',
+    value: 525,
+    conmutationBasicLevelList: [
+      {
+        id: 29,
+        name: '950',
+        value: 950,
+        isolationBasicLevelsList: [
+          {
+            id: 44,
+            name: '1175',
+            value: 1175,
+          },
+          {
+            id: 45,
+            name: '1300',
+            value: 1300,
+          },
+        ],
+      },
+      {
+        id: 30,
+        name: '1050',
+        value: 1050,
+        isolationBasicLevelsList: [
+          {
+            id: 46,
+            name: '1300',
+            value: 1300,
+          },
+          {
+            id: 47,
+            name: '1425',
+            value: 1425,
+          },
+        ],
+      },
+      {
+        id: 31,
+        name: '1175',
+        value: 1175,
+        isolationBasicLevelsList: [
+          {
+            id: 48,
+            name: '1425',
+            value: 1425,
+          },
+          {
+            id: 49,
+            name: '1550',
+            value: 1550,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 17,
+    name: '765',
+    value: 765,
+    conmutationBasicLevelList: [
+      {
+        id: 32,
+        name: '1300',
+        value: 1300,
+        isolationBasicLevelsList: [
+          {
+            id: 50,
+            name: '1675',
+            value: 1675,
+          },
+          {
+            id: 51,
+            name: '1800',
+            value: 1800,
+          },
+        ],
+      },
+      {
+        id: 33,
+        name: '1425',
+        value: 1425,
+        isolationBasicLevelsList: [
+          {
+            id: 52,
+            name: '1800',
+            value: 1800,
+          },
+          {
+            id: 53,
+            name: '1950',
+            value: 1950,
+          },
+        ],
+      },
+      {
+        id: 34,
+        name: '1550',
+        value: 1550,
+        isolationBasicLevelsList: [
+          {
+            id: 54,
+            name: '1950',
+            value: 1950,
+          },
+          {
+            id: 55,
+            name: '2100',
+            value: 2100,
+          },
+        ],
+      },
+    ],
+  },
+];
