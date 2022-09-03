2a44c8c63b188d6a4f64ef38f332823113ef0dd0
Diego Mejia <diegorobertomejiasalinas@gmail.com>
2022-09-03 13:14:03 -0500

    Mechanical Calculation completed form and getting data from Storage Service

--- src/app/modules/mechanical-calculation/mechanical-calculation.component.ts
+++ src/app/modules/mechanical-calculation/mechanical-calculation.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { StorageService } from 'src/app/core/services/storage.service';
 
 @Component({
   selector: 'app-mechanical-calculation',
@@ -6,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mechanical-calculation.component.scss'],
 })
 export class MechanicalCalculationComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private readonly storageService: StorageService,
+    private readonly router: Router
+  ) {
+    this.form = this.fb.group({
+      windSpeed: [null, [Validators.required]],
+      chainHardwareWeight: [null, [Validators.required]],
+      conductorsPerPhaseNumber: [null, [Validators.required]],
+      verticalStressTransmitted: [null, [Validators.required]],
+      maximumHorizontalTension: [null, [Validators.required]],
+    });
+
+    this.storageService.mechanicalCalculation
+      .pipe(
+        map((data) => {
+          if (data) {
+            this.form.patchValue(data);
+          }
+        })
+      )
+      .subscribe();
+  }
 
   ngOnInit() {}
 
+  saveForm() {
+    this.storageService.setMechanicalCalculationData(this.form.getRawValue());
+    this.storageService.setMechanicalCalculationIsComplete(true);
+
+    this.router.navigate(['/']);
+  }
 }
